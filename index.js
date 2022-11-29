2750be3d4d18bb5af6d9585457c601e4f7d83486
Noel <36994257+YeyeIO@users.noreply.github.com>
2022-11-28 21:23:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function animateTitle(Title = "noel56983873", delay = 50) {
+    let counter = 0;
+    let direction = true;
+    aniTitle = setInterval(function () {
+        if (counter == Title.length)
+            direction = false;
+        if (counter == false)
+            direction = true;
+        counter = (direction == true) ? ++counter : --counter;
+        newtitle = (counter == 0) ? " " : Title.slice(0, counter);
+        document.title = newtitle;
+    }, delay)
+}
+
+animateTitle()
